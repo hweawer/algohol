6a2c6991dc9b4ff300c11cd2a0b18e1f30b50fd1
agusverdas <alexandergusakovsky@gmail.com>
2021-12-24 15:46:26 +0300

    Leetcode daily

--- leetcode/src/main/java/edu/agus/leetcode/medium/MergeIntervals.java
+++ leetcode/src/main/java/edu/agus/leetcode/medium/MergeIntervals.java
@@ -25,13 +25,4 @@ public class MergeIntervals {
     }
     return result;
   }
-
-  public static void main(String[] args) {
-    merge(new int[][] {
-        {1, 3},
-        {2, 6},
-        {8, 10},
-        {15, 18}
-    });
-  }
 }
